4f82e52c3202db083ec71144a5d749b63fe3f757
Yannick <Yannickhantaniaina@gmail.com>
2026-09-20 13:12:04 -0400

    Refined hat details & Background - Prototype 1

--- prototyping-instructions/prototype-1/js/script.js
+++ prototyping-instructions/prototype-1/js/script.js
@@ -23,7 +23,16 @@ function draw(){
 function drawbackground() {
     background(40,40,70)
     fill(50,25,0)
-    rect(0, 1400, 3508, 1200)
+    rect(0, 1330, 3508, 1200)
+
+    push()
+    stroke(0)
+    strokeWeight(2)
+    line(900,1330,3508,1330)
+    pop()
+    
+    fill(30,20,0)
+    quad(1250,1455,1570,1455,1451,1665,963,1665)
 }
 
 function hatShape(){
@@ -35,17 +44,45 @@ function hatShape(){
     triangle(1410,1300,1250,1455,1570,1455);
     ellipse(1410,1455,320,55);
 
+    push()
     fill(199,199,199)
     stroke(0)
     strokeWeight(1)
     circle(1410,1380,30);
+    pop()
+
+    // This is the shadow over the hat 
+
+    fill(0,0,0,100);
+    triangle(1395,966,1250,1455,1545,1455);
+    arc(1410,1455,320,55,TWO_PI,PI);
+
+}
+
+function drawWindows(){
+
+    push()
+    fill(90,150,200)
+    square(1650,990,150)
+    square(1650,810,150)
+    square(1830,990,150)
+    square(1830,810,150)
+    pop()
+
+
+    push()
+    stroke(0)
+    strokeWeight(2)
+    fill(255,255,255,20)
+    square(1600,760,430)
+    pop()
 
 }
 
  
 drawbackground();
 hatShape();
-
+drawWindows();
 // displays mouse coordinates on canvas
 
 fill(255,255,255);
